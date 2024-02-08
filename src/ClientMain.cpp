07ef7d9e3d5e20d205d4816c491a6e6c3f210b1e
peter <yeyoung0909@163.com>
2024-02-08 14:50:42 +0800

    add completion part1.

--- src/ClientMain.cpp
+++ src/ClientMain.cpp
@@ -12,6 +12,10 @@ const std::string VERSION = "0.1.1";
 
 inline void showPrompt();
 void showBanner();
+char** myCompletion(const char *text, int start, int end);
+char * command_generator(const char *text, int state);
+
+char** keyWords;
 
 
 int main(int argc, char* argv[]){
@@ -82,6 +86,7 @@ int main(int argc, char* argv[]){
         client.runInteractive("version()");
         struct passwd *pw = getpwuid(geteuid());
         string historyFile = string(pw -> pw_dir) + string("/.ddbclient_history");
+        rl_attempted_completion_function = myCompletion;
         //just read last 100 command history
         read_history_range (historyFile.c_str(), 0, 99);
         vector<string> keyWords;
@@ -139,4 +144,45 @@ support email: support@dolphindb.com
 -----------------------------------------------------------
 )";
     std::cout << banner << std::endl;
+}
+
+char** myCompletion(const char *text, int start, int end){
+ char **matches;
+
+  matches = (char **)NULL;
+
+  /* If this word is at the start of the line, then it is a command
+     to complete.  Otherwise it is the name of a file in the current
+     directory. */
+  if (start == 0)
+    matches = rl_completion_matches (text, command_generator);
+
+  return (matches);
+}
+
+    
+char * command_generator(const char *text, int state){
+  static int list_index, len;
+  char *name;
+
+  /* If this is a new word to complete, initialize now.  This includes
+     saving the length of TEXT for efficiency, and initializing the index
+     variable to 0. */
+  if (!state)
+    {
+      list_index = 0;
+      len = strlen (text);
+    }
+
+  /* Return the next name which partially matches from the command list. */
+  while (name = keyWords[list_index])
+    {
+      list_index++;
+
+      if (strncmp (name, text, len) == 0)
+        return (dupstr(name));
+    }
+
+  /* If no names matched, then return NULL. */
+  return ((char *)NULL);
 }
